013d4768acaf47a3a4aa99337ab07f00256063d8
saikrishna1235 <154864664+saikrishna1235@users.noreply.github.com>
2026-01-22 22:35:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 // Middleware
 app.use(cors({
   origin: [
-    "https://arms-assignment-and-resources-management-system.vercel.app",
+    "https://arms-assignment-and-resources-manag.vercel.app",
     "http://localhost:5500",
     "http://127.0.0.1:5500"
   ],
