073751e1f0733ede4a4764ca7134ec6f8a59e931
saikrishna1235 <154864664+saikrishna1235@users.noreply.github.com>
2026-01-22 19:21:46 +0530

    Refactor MySQL connection to use MYSQL_URL

--- db.js
+++ db.js
@@ -1,16 +1,10 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect((err) => {
   if (err) {
-    console.error("❌ MySQL connection failed:", err.message);
+    console.error("❌ MySQL connection failed:", err);
   } else {
     console.log("✅ MySQL Connected Successfully");
   }
