467fed08d23ba2c0fb9620fa90059c1203ec3fc4
K Sai Krishna <154864664+saikrishna1235@users.noreply.github.com>
2026-05-11 11:42:00 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
-const mysql = require("mysql2");
+const { Pool } = require("pg");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "charansai1234@",
-  database: "node_lms"
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error("❌ MySQL connection failed:", err.message);
-  } else {
-    console.log("✅ MySQL Connected Successfully");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 
-module.exports = db;
+pool.connect((err) => {
+  if (err) {
+    console.error("❌ PostgreSQL connection failed:", err.message);
+  } else {
+    console.log("✅ PostgreSQL Connected Successfully");
+  }
+});
+
+module.exports = pool;
